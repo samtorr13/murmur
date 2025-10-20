7907839acb971bfb442a38e8db0c5d8e4927f30b
Sam <80631546+samtorr13@users.noreply.github.com>
2025-10-20 18:01:24 -0400

    Username-Change
    solucionado problema que no permitia cambiar el username desde la pantalla de selección de perfil
    
    ahora al escribir el nombre de usuario en su respectivo campo, este validará si el usuario existe, o si cumple con el minimo de caracteres

--- core/views.py
+++ core/views.py
@@ -1,3 +1,27 @@
 from django.shortcuts import render
 
-# Create your views here.
+
+from django.http import HttpResponse
+from django.template.loader import render_to_string
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
+
+def validar_username(request):
+    username = request.GET.get("usrname", "").strip()
+    valid = True
+    message = "Disponible ✅"
+
+    if not username:
+        valid = False
+        message = "no"
+    elif User.objects.filter(username__iexact=username).exists():
+        valid = False
+        message = "Este nombre ya está en uso"
+
+    html = render_to_string("components/username_feedback.html", {
+        "valid": valid,
+        "message": message
+    })
+
+    return HttpResponse(html)
--- murapp/urls.py
+++ murapp/urls.py
@@ -9,14 +9,20 @@ from murapp import settings
 from . import views
 urlpatterns = [
     path('admin/', admin.site.urls),
+
     path('', views.home, name='home'),
+    path('', include('user_profile.urls')),
+    path('', include('core.urls')),
+
     path('accounts/', include('allauth.urls')),
     path('logout/', LogoutView.as_view(next_page='/'), name='logout'),
+    path('welcome/', views.welcome_wizard, name='welcome'),
+
     path('post/like/<int:post_id>/', views.like_post, name='like_post'),
-    path('', include('user_profile.urls')),
     path('posts/comment/<int:post_id>/', view=views.comment_as_view, name='comment_as_view'),
     path('posts/report/<int:post_id>/', view=views.report_post, name='report_post'),
-    path('welcome/', views.welcome_wizard, name='welcome')
+
+    
 ]
 
 if settings.DEBUG:
